f36a5e382691ef7796c1b8a141c1c32ac96fb817
Yusuke Uchiyama <uchiyama@icepp.s.u-tokyo.ac.jp>
2022-08-12 19:07:16 +0200

    Specify protocol version for pickle.

--- csv2pickle.py
+++ csv2pickle.py
@@ -11,5 +11,5 @@ outputfile = os.path.splitext(outputfile)[0] + '.pkl' + '.gz'
 # outputfile = os.path.splitext(outputfile)[0] + '.pkl' + '.bz2'
 
 csv = pandas.read_csv(filename, header=None)
-csv.to_pickle(outputfile)
+csv.to_pickle(outputfile, protocol = 4) # when protocol 5 is supported in Google Colab, remove the protocol specification
 #pickle.dump(csv, open(outputfile, 'w'))
